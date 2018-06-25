eda319e68f415300830001e605061a82d804d549
Lianne Lairmore <lianne.lairmore@gmail.com>
2018-06-25 00:06:09 -0400

    add menu

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,5 @@
 import System.IO
+import Text.Read
 
 import KnitStitches
 
@@ -11,9 +12,35 @@ writePattern fileName [] = writeFile fileName ""
 writePattern fileName pattern = writeFile fileName (show pattern)
 
 
+menu :: [String] -> IO Int
+menu [] = undefined
+menu options = menu' options 1
+    where
+        menu' [] _ = do
+            strChoice <- getLine
+            let choice = readMaybe strChoice :: Maybe Int in case choice of
+                Just n -> return n
+                _ -> return 0
+        menu' (s:tl) n = do
+            putStrLn ((show n) ++ ") " ++ s)
+            menu' tl (n+1)
 
 options = [
-    "Enter Row"
+    "Read",
+    "Write",
+    "Show",
+    "Quit"]
+
 
 main = do
     putStrLn "Test"
+    loop
+
+loop = do
+    choice <- menu options
+    if choice < 1 || choice > length options then do
+        putStrLn "Not an option"
+        loop
+    else do
+        putStrLn ("Chose " ++ (show choice) ++ ") " ++ (options !! (choice - 1)))
+        if choice == length options then putStrLn "Good Bye" else loop
