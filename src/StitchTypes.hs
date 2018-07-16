245cc1da1b13595ff27c641da30534542f8ddd86
Lianne Lairmore <lianne.lairmore@gmail.com>
2018-07-16 08:32:09 -0400

    Extended a bunch, added more stitches

--- src/StitchTypes.hs
+++ src/StitchTypes.hs
@@ -1,11 +1,21 @@
 module StitchTypes where
 
+class Reversable a where
+    other :: a -> a
+    alternating :: a -> Int -> [a]
+    alternating _ 0 = []
+    alternating a n = a : (alternating (other a) (n-1))
+
 -- Type that says gives instruction
 class Instructable a where
     instr :: a -> String
+    commaInstr :: [a] -> String
+    commaInstr (a:[]) = instr a
+    commaInstr (a:as) = ((instr a) ++", ") ++ concatInstr as
     concatInstr :: [a] -> String
-    concatInstr (a:[]) = instr a
-    concatInstr (a:as) = ((instr a) ++", ") ++ concatInstr as
+    concatInstr as = concat $ instr <$> as
+    expanded :: a -> String
+    expanded = instr
 
 -- Type that can be a stitch
 class (Eq a, Show a, Read a, Instructable a) => Stitches a where
@@ -17,36 +27,70 @@ class (Eq a, Show a, Read a, Instructable a) => Stitches a where
 -- Base stitches of knit and purl
 data Base = Knit | Purl deriving (Eq, Show, Read)
 
+instance Reversable Base where
+    other Knit = Purl
+    other Purl = Knit
+
 -- Base is an instance of instructable?
 instance Instructable Base where
     instr Knit = "k"
     instr Purl = "p"
 
+    expanded Knit = "knit"
+    expanded Purl = "purl"
+
+
 -- Side is the side of the loop to put the needle through
 data Side = Front | Back deriving (Eq, Show, Read)
-instance Instructable ToOne where
-    instr (Dec d) = instr d
-    instr (One o) = instr o
 
-instance Stitches ToOne where
-    uses (Dec d) = uses d
-    uses _ = 1
-    makes _ = 1
-    conforms a = makes a == 1
+instance Reversable Side where
+    other Front = Back
+    other Back = Front
 
+instance Instructable Side where
+    instr Front = "f"
+    instr Back = "b"
+    expanded Front = "front"
+    expanded Back = "back"
 
 -- Break up types to ... maybe these should be functions
 -- 1 -> 1
 -- + -> 1
 -- 1 -> +
--- 2+ -> 2+
+-- 0 -> +
 -- * -> *
 -- + -> +
 
 -- combine all that make 1
 data ToOne = Dec ManyToOne
-             | One OneToOne deriving (Eq, Show, Read)
+           | ToOne OneToOne deriving (Eq, Show, Read)
 
+
+instance Instructable ToOne where
+    instr (Dec d) = instr d
+    instr (ToOne o) = instr o
+
+instance Stitches ToOne where
+    uses (Dec d) = uses d
+    uses (ToOne o) = uses o
+    makes (Dec d) = makes d
+    makes (ToOne o) = makes o
+    conforms a = makes a == 1
+
+
+data FromOne = Incr OneToMany
+             | FromOne OneToOne deriving (Eq, Show, Read)
+
+instance Instructable FromOne where
+    instr (Incr i) = instr i
+    instr (FromOne f) = instr f
+
+instance Stitches FromOne where
+    uses (Incr i) = uses i
+    uses (FromOne f) = uses f
+    makes (Incr i) = makes i
+    makes (FromOne f) = makes f
+    conforms s = uses s == 1
 
 -- 1 -> 1
 data OneToOne = Stitch Base Side
@@ -55,12 +99,13 @@ data OneToOne = Stitch Base Side
 instance Instructable OneToOne where
     instr (Stitch b Front) = instr b
     instr (Stitch b Back) = (instr b) ++ "tbl"
-    instr (Slip Knit) = "sl knit-wise"
-    instr (Slip Purl) = "sl purl-wise"
+    instr (Slip b) = "sl "++(expanded b) ++ "-wise"
 
 instance Stitches OneToOne where
-    uses _ = 1
-    makes _ = 1
+    uses (Stitch _ _) = 1
+    uses (Slip _) = 1
+    makes (Stitch _ _) = 1
+    makes (Slip _) = 1
     conforms a = uses a == 1 && makes a == 1
 
 -- + -> 1
@@ -71,45 +116,77 @@ data ManyToOne = Tog Int Base Side
 instance Instructable ManyToOne where
     instr (Tog n b s) = (instr (Stitch b s)) ++ (show n) ++ "tog"
     instr (RHTog n b s) = "sl " ++ (show n) ++ (instr (Stitch b s)) ++ " slipped stitches"
-    instr (PassOver sts st) = undefined --("first " ++ (concatInstr sts) ++ " then "
-                              -- ++ (instr st) ++ "pass previous stitches over last stitch")
+    instr (PassOver sts st) = ("first " ++ (commaInstr sts) ++ " then "
+                               ++ (instr st) ++ "pass previous stitches over last stitch")
 
 instance Stitches ManyToOne where
     uses (Tog n _ _) = n
     uses (RHTog n _ _) = n
-    uses (PassOver sts st) = undefined -- foldl (+) 0 (uses <$> sts) + (uses st)
+    uses (PassOver sts st) = foldl (+) 0 (uses <$> sts) + (uses st)
+    makes (PassOver _ st) = makes st
     makes _ = 1
-    conforms = undefined
+    conforms s = uses s > 1 && makes s == 1
 
 -- 1 -> +
-data OneToMany = FrontBack Int
-               | KnitPurl Int deriving (Eq, Show, Read)
+data OneToMany = FrontBack Base Side Int
+               | KnitPurl Base Side Int deriving (Eq, Show, Read)
 
 instance Instructable OneToMany where
-    instr = undefined
+    expanded (FrontBack b start n) = (expanded b) ++ " into " ++ (expanded start) ++
+                                     " and " ++ (expanded $ other start) ++
+                                     " of next stitch " ++ (show n) ++ " times"
+    expanded (KnitPurl s Front n) = (expanded s) ++ " and " ++ (expanded $ other s) ++
+                                    " into next stitch " ++ (show n) ++ " times"
+    instr (FrontBack b s n) = (instr b) ++ (concatInstr $ alternating s n)
+    instr (KnitPurl s Front n) = (concatInstr $ alternating s n) ++ " into next stitch"
+    instr (KnitPurl s Back n) = (concatInstr $ alternating s n) ++ " tbl of next stitch"
 
 
 instance Stitches OneToMany where
-    uses = undefined
-    makes = undefined
-    conforms = undefined
+    uses _ = 1
+    makes (FrontBack _ _ n) = n
+    makes (KnitPurl _ _ n) = n
+    conforms s = uses s == 1 && makes s > 1
 
--- 2 -> 2+
-data TwoToMany = Yo Stitch Stitch
-               | Make Stitch Side Side Stitch deriving (Eq, Show, Read)
+-- 0 -> +
+data ZeroToMany = Yo
+               | Make Side FromOne
+               | Below FromOne deriving (Eq, Show, Read)
 
-instance Instructable TwoToMany where
-    instr = undefined
+instance Instructable ZeroToMany where
+    instr Yo = "yo"
+    instr (Make s (FromOne st@(Stitch b side))) = "m1" ++ lean ++ stitch
+        where
+            lean = case s of
+                    Front -> "l"
+                    Back -> "r"
+            stitch = case b of
+                        Knit -> case side of
+                                    Front -> ""
+                                    _ -> instr st
+                        _ -> instr st
+    instr (Make s st) = "insert between needles from " ++ (expanded s) ++ " to " ++
+                        (expanded $ other s) ++ " and " ++ (instr st)
+    instr (Below st) = (instr st) ++ " into stitch below next stitch"
+    expanded (Make s st) = "insert needle into yarn between stitches from the " ++
+                           (expanded s) ++ " and " ++ (expanded st)
+    expanded s = instr s
 
-instance Stitches TwoToMany where
-    uses = undefined
-    makes = undefined
-    conforms = undefined
+instance Stitches ZeroToMany where
+    uses _ = 0
+    makes Yo = 1
+    makes (Make _ st) = makes st
+    makes (Below st) = makes st
+    conforms s = uses s == 0 && makes s > 0
+
+-- * -> * or + -> +?
+data ManyToMany = Seq [Stitch]
+                | Cable [] [] deriving (Eq, Show, Read)
 
 data Stitch = Base OneToOne
             | Together ManyToOne
             | Into OneToMany
-            | Makes TwoToMany deriving (Eq, Show, Read)
+            | Makes ZeroToMany deriving (Eq, Show, Read)
 
 
 instance Instructable Stitch where
